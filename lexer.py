ae078dc25b6c1a9a2bcc8669b748a3ec8ee20adc
Andrew Ang <andrewang123@hotmail.com>
2017-11-05 19:57:53 -0800

    checks if valid input file

--- lexer.py
+++ lexer.py
@@ -3,9 +3,26 @@
 #Nov 5th 2017
 #Lexical Analyzer, the purpose of this program is to create a lexical analyzer
 # for the mini - power grammmar
+import os #check if valid file
+import sys #used to end program
+
+def getInput():
+
+
+    inputFile = input("prompt>  python lexer.py ")
+
+    #check if the file is valid
+    if not os.path.exists(inputFile):
+        print("Input File is invalid")
+        sys.exit()
+
+    print(inputFile)
+    #open file using context manager
+    with open(inputFile, 'r') as file: #read the contents of file
+        print(file.read())
+        fileContents = file.read()
+
+
 
 #main
-print("prompt>")
-intput()
-person = input('prompt> python ')
-print('Hello', person)
+getInput()
