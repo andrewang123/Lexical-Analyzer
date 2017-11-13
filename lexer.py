c9a31272de74f57643c432f3ceac9174d1d5f136
Andrew Ang <andrewang123@hotmail.com>
2017-11-13 08:50:41 -0800

    Added another input file and made the program read line by line

--- lexer.py
+++ lexer.py
@@ -2,9 +2,10 @@
 #Prof Arias
 #Nov 5th 2017
 #Lexical Analyzer, the purpose of this program is to create a lexical analyzer
-# for the mini - power grammmar
-import os #check if valid file
-import sys #used to end program
+#for the mini - power grammar
+import os
+import sys
+import re
 
 #Checks if there is a file that can be opened, ends program if not
 def checkValid():
@@ -17,14 +18,30 @@ def checkValid():
     print(inputFile)
     return inputFile
 
+#Go through line by line in the file
+#inputFile refers to the string of the input file
 def goThrough(inputFile):
     # open file using context manager dont need to close
     # closes by itself
     with open(inputFile, 'r') as file:
-        print(file.read())
-        fileContents = file.read()
+
+        #print(file.read())
+        fileContents = file.readline()
+        ouputTokens(fileContents)
+
+#Passes each line of the file and processes it
+def ouputTokens(lineProcess):
+    print("Hello There")
+    print(lineProcess)
+    sys.exit()
 
 #main
 inputFile = checkValid()
 goThrough(inputFile)
 
+'''
+TO DO LIST:
+1. make sure there is no Capitals
+2. output into output file
+3. make so works in terminal
+'''
