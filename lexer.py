5c16e3f32631e1ead454a381ac84505d9f9cbc1c
Andrew Ang <andrewang123@hotmail.com>
2017-11-16 17:04:44 -0800

    made it output to output file

--- lexer.py
+++ lexer.py
@@ -1,14 +1,16 @@
 #Andrew Ang
 #Prof Arias
 #Nov 5th 2017
-#Lexical Analyzer, the purpose of this program is to create a lexical analyzer
-#for the mini - power grammar
+#Lexical Analyzer, the purpose of this program is to create a
+#lexical analyzer for the mini - power grammar
 
 import os
 import sys
 import re
 
 #Checks if there is a file that can be opened, ends program if not
+#Parameters: N/A
+#Returns: the name of the inputfile
 def checkValid():
     inputFile = input("prompt>  python lexer.py ")
 
@@ -16,145 +18,124 @@ def checkValid():
     if not os.path.exists(inputFile):
         print("Input File is invalid")
         sys.exit()
-    print(inputFile)
+    print("Processing input file " + inputFile)
     return inputFile
 
-#Go through line by line in the file
-#inputFile refers to the string of the input file
+#Go through entire file
+#Parameters: inputFile refers to the string of the input file
+#Returns: N/A
 def goThroughLine(inputFile):
     # open file using context manager dont need to close
     # closes by itself
     totalNumTokens = 0
+    outputFileName = inputFile[:-4] + ".out"
     with open(inputFile, 'r') as file:
+        outFile = open(outputFileName,'w')
         for line in file:
             #print(file.read())
             #fileContents = file.readline()
-            totalNumTokens += createTokens(line)
-        print(totalNumTokens)
+            totalNumTokens += createTokens(line, outFile)
+        print(str(totalNumTokens) + " tokens produced")
+        print("Result in file " + outputFileName)
+        outFile.close()
 
 #Passes each line of the file and processes it
-def createTokens(lineProcess):
+#Parameters: lineProcess which is the line being processed
+#Returns: total tokens for that specific line
+def createTokens(lineProcess, outputFile):
     lenOfString = len(lineProcess)
-    #print(lenOfString)
-
-    #print(lineProcess)
-
-    potentialToken = ""
+    potentialToken = "" #string to construct the token
     tokens = [] #create a list of tokens
-    isString = False;
+    isString = False; #check for strings ex) "The result is"
     seenQuotation = 0
     totalTokens = 0
     for x in range(0, lenOfString): #check char by char
-    #for char in lineProcess:
         #print(lineProcess[x])
         if(lineProcess[x] != " "):
-            if(lineProcess[x] == "\""):
+            if(lineProcess[x] == "\""): #check for '"'
                 isString = True
                 seenQuotation += 1
                 if (seenQuotation == 2):
                     isString = False
-            else: # make sure " is not included
+                    seenQuotation = 0
+            else:
                 potentialToken += lineProcess[x]
-            # check if there is a capital
-
-            #at the end
+            #account for last character in line
             if (x == lenOfString - 1):
                 tokens.append(potentialToken)
                 totalTokens += 1
                 # change potentialToken to empty
                 potentialToken = ""
         else: # There is a space
-            if (not(isString)): #if not a string than erase and get ready for next token
-                #check if it is a valid string
-                # add it to the list
+            if (not(isString)): #not a string
+                 # add it to the list
                 tokens.append(potentialToken)
                 totalTokens += 1
                 # change potentialToken to empty
                 potentialToken = ""
-            else:
-                potentialToken += lineProcess[x] #add a space if it is a string
-
-    #print(lineProcess[lenOfString-2])
-    print(tokens)
-    checkSyntax(tokens)
+            else: #it is a string
+                potentialToken += lineProcess[x] #add a space
+    #print(tokens)
+    checkSyntax(tokens, outputFile)
     return totalTokens
 
-def checkSyntax(tokens):
+#Check if the tokens that are produced are part of the Rules
+#Parameters: tokens the list of tokens, name of the file
+#Returns: N/A
+def checkSyntax(tokens, outputFile):
     #Check for errors
     for token in tokens:
         #print(token)
         if (token == "="):
-            print("ASSIGN")
+            outputFile.write("ASSIGN\n")
         elif (token == ";\n" or token == ";"):
-            print("SEMICOLON")
-        elif (re.search('[a-z](([a-z]*)|([0-9]*))[$|#|%]', token)): #first character is lowercase letter
-            print("ID    ", end="")
-            print(token[:-1], "    ", end="") #everything except for the last
+            outputFile.write("SEMICOLON\n")
+        elif (re.search('^[a-z](([a-z]*)|([0-9]*))[$|#|%]$', token)): #RegEx for ID
+            outputFile.write("ID    ")
+            outputFile.write(token[:-1] + "   ") #everything except for the last
             if(token.endswith("$")):
-                print("STRING")
+                outputFile.write("STRING\n")
             elif(token.endswith("#")):
-                print("INTEGER")
+                outputFile.write("INTEGER\n")
             elif(token.endswith("%")):
-                print("REAL")
-                #([-|+]?)
+                outputFile.write("REAL\n")
         elif (re.search('^[-|+]?[0-9]+$',token)): # int-const
-            print("INT_CONST", end="  ")
-            print(token)
-        elif (re.search('^[-|+]?[0-9]+\.[0-9]*$',token)): # real const
-            print("REAL_CONST", end="  ")
-            print(token)
+            outputFile.write("INT_CONST   ")
+            outputFile.write(token + "\n")
+        elif (re.search('^[-|+]?[0-9]+\.[0-9]*$',token)): # real-const
+            outputFile.write("REAL_CONST  ")
+            outputFile.write(token + "\n")
         elif (token == '+' ):
-            print("PLUS")
+            outputFile.write("PLUS\n")
         elif (token == '-'):
-            print("MINUS")
+            outputFile.write("MINUS\n")
         elif (token == '*'):
-            print("TIMES")
+            outputFile.write("TIMES\n")
         elif (token == '/'):
-            print("DIVIDE")
+            outputFile.write("DIVIDE\n")
         elif (token == "PRINT"):
-            print("PRINT")
+            outputFile.write("PRINT\n")
         elif (" " in token): #token contains a space
-            print("STRING:", end="     ") # For loop here keep printing until endswith "
-            print(token)
+            outputFile.write("STRING:    ")
+            outputFile.write(token + "\n")
         elif (token == '('):
-            print("LEFT_PAREN")
+            outputFile.write("LEFT_PAREN  ")
         elif (token == ')'):
-            print("RIGHT_PAREN")
+            outputFile.write("RIGHT_PAREN  ")
         elif (token == '^'):
-            print("EXPONENT")
+            outputFile.write("EXPONENT  ")
         else:
             print("Error, File does not match grammar.")
-        #^
+            sys.exit()
         # make sure that the ending token is no semi colon
 
-
-
         #if there are any string left in the list produce an error
-
-    #should work for any string starting with letter
-    #followed by any combination of number and letter
-
-#read char by char!!!!
-    #matchID = re.search('[a-z](([a-z]*)|([0-9]*))[$|#|%]', lineProcess)
-    #matchType = re.search('[$|#|%]', lineProcess)
-
-    #match
-    #if matchID:
-    #    print("ID    ", end="")
-        #print(re.search('[a-z]', lineProcess))
-    #    print(matchID.group())
-        #lastChar =
-
-
-    #sys.exit()
-
 #main
 inputFile = checkValid()
 goThroughLine(inputFile)
 
 '''
 TO DO LIST:
-1. make sure there is no Capitals
 2. output into output file
 3. make so works in terminal
 '''
